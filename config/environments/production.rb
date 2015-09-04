42679c768ee29d76aa61b2e6ea5db47414fd28a7
Donavan White <digi.cazter@gmail.com>
2015-09-04 17:31:30 -0600

    Styled devise pages and added a logo, added omniauth to devise for google+ login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.mandrillapp.com",
+    :user_name => ENV["MANDRILL_API_USERNAME"],
+    :password => ENV["MANDRILL_API_KEY"],
+    :port => 587,
+    :host => "http://todo-tracker.herokuapp.com"
+  }
+  config.action_mailer.default_url_options = { :host => 'http://todo-tracker.herokuapp.com' }
 end
