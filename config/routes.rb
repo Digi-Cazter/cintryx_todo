9f39bb57cadc2a63f643cfba92397d87ac27a480
Donavan White <digi.cazter@gmail.com>
2015-09-04 17:01:00 -0600

    Added omniauth setup for google to devise and user routing for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   resources :lists
-  root to: 'lists#index'
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do
+    authenticated :user do
+      root 'lists#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root to: "devise/sessions#new", as: :unauthenticated_root
+    end
+  end
 end
